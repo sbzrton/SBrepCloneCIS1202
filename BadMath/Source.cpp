3346991fff833bf65324923b97e97b9d39cd7a18
Sha'maria Barton <shbarton21@gmail.com>
2022-03-14 10:45:58 -0400

    Fixed the program by getting rid of logic errors located in the function (under main) and in the user input (located in main). Added function prototype and references while also moving the function itself under main. Lastly, I amended the way the function returned the average.

--- BadMath/Source.cpp
+++ BadMath/Source.cpp
@@ -2,22 +2,32 @@
 #include <iomanip>
 using namespace std;
 
-float average(int i1, int i2, int i3)
-{
-	return i1 + i2 + i3 / 3.0f;
-}
+
+//Function Prototype
+float average(int&, int&, int&);
 
 int main()
 {
 	int n1 =0 , n2 = 0, n3 = 0;
 	cout << "This program calculates the average of three numbers." << endl;
-	cout << "First number: "; cin >> n1;
+	cout << "\nFirst number: "; cin >> n1;
 	cout << "Second number: "; cin >> n2;
-	cout << "Third number: "; cin >> n2;
+	cout << "Third number: "; cin >> n3;
 
 	float a = average(n1, n2, n3);
 
-	cout << setprecision(1) << fixed << "The average is " << a << endl;
+	cout << setprecision(1) << fixed << "\n\nThe average is " << a << endl;
 
+
+	cin.ignore(INT_MAX);
 	return 0;
+
+}
+
+//Calling Function
+float average(int &i1, int &i2, int& i3)
+{
+	float avg = 0;
+	avg = (i1 + i2 + i3) / 3.0;
+	return avg;
 }
